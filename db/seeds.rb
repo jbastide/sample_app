cde8ef4f5f2df772affa0b3d72ea3dc69c836329
Jesse Bastide <jbastide001@yahoo.com>
2015-12-23 19:31:06 +0100

    Finish user edit, update, index, and destroy actions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+User.create!(name:  "Jesse Bastide",
+            email: "jesse@jesse.com",
+            password:              "password",
+            password_confirmation: "password",
+            admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
